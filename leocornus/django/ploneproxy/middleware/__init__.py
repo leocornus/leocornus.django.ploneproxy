a4270470af18b751845a3d75e50f30fc8cad23bf
Sean Chen <sean.chen@leocorn.com>
2010-05-20 18:18:46 +0000

    move the code for setting default values to a subpackage, this will avoid the mod_python import issue. - there should be a better way for this.

--- leocornus/django/ploneproxy/middleware/__init__.py
+++ leocornus/django/ploneproxy/middleware/__init__.py
@@ -12,6 +12,17 @@ from django.utils import translation
 __author__ = "Sean Chen"
 __email__ = "sean.chen@leocorn.com"
 
+# using the Plone site's default cookie name.
+settings.PLONEPROXY_COOKIE_NAME = getattr(settings, 'PLONEPROXY_COOKIE_NAME',
+                                          '__ac')
+
+# using localhost:8080
+settings.PLONEPROXY_AUTHEN_URL = getattr(settings, 'PLONEPROXY_AUTHEN_URL',
+                                         'http://localhost:8080/Plone/login_form')
+
+# the default language field name
+settings.PLONEPROXY_LANG_FIELD_NAME = getattr(settings, 'PLONEPROXY_LANG_FIELD_NAME',
+                                              'ldp_lang')
 class LocaleMiddleware(object):
     """
     This middleware is copied from the default Django LocaleMiddleware.
